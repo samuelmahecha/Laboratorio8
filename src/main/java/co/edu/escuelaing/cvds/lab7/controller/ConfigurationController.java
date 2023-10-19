addef0d5543a52b824c92530f0efbad2c52cf706
unknown <jose.mahecha@labinfo.is.escuelaing.edu.co>
2023-10-19 17:46:12 -0500

    inicio lab8

---
+++ src/main/java/co/edu/escuelaing/cvds/lab7/controller/ConfigurationController.java
@@ -0,0 +1,26 @@
+package co.edu.escuelaing.cvds.lab7.controller;
+
+import co.edu.escuelaing.cvds.lab7.model.ToDoItem;
+import co.edu.escuelaing.cvds.lab7.service.ConfigurationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class ConfigurationController {
+
+    @Autowired
+    ConfigurationService configurationService;
+
+    @GetMapping("/example")
+    public String example(Model model) {
+        model.addAttribute("premio", configurationService.getPremio());
+        return "example";
+    }
+
+}
